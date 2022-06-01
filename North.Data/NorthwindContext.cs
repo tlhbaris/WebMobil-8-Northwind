4a28ef468226ba1a1edd46ab64b6efec78ef8fec
tlhbaris <103104761+tlhbaris@users.noreply.github.com>
2022-06-01 11:40:43 +0300

    .

--- North.Data/NorthwindContext.cs
+++ North.Data/NorthwindContext.cs
@@ -5,10 +5,7 @@ namespace North.Data
 {
     public partial class NorthwindContext : DbContext
     {
-        public NorthwindContext()
-        {
-        }
-
+       
         public NorthwindContext(DbContextOptions<NorthwindContext> options)
             : base(options)
         {
@@ -42,14 +39,7 @@ namespace North.Data
         public virtual DbSet<Supplier> Suppliers { get; set; } = null!;
         public virtual DbSet<Territory> Territories { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-            }
-        }
+        
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
