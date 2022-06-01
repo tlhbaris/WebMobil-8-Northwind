51f0e643a604ef9744226208034bb96a1bc4a834
tlhbaris <103104761+tlhbaris@users.noreply.github.com>
2022-06-01 14:58:27 +0300

    PaymentService oluşturuldu

---
+++ North.Businesss/Services/Payment/IPaymentService.cs
@@ -0,0 +1,11 @@
+﻿using North.Core.Payments;
+
+namespace North.Businesss.Services.Payment
+{
+    public interface IPaymentService
+    {
+        InstallmentModel CheckInstallments(string binNumber, decimal price);
+        PaymentResponseModel Pay(PaymentModel model);
+
+    }
+}
---
+++ North.Businesss/Services/Payment/IyzicoPaymentService.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using Iyzipay;
+using Iyzipay.Model;
+using Iyzipay.Request;
+using Microsoft.Extensions.Configuration;
+using MUsefulMethods;
+using North.Core.Payments;
+using System.Globalization;
+
+namespace North.Businesss.Services.Payment
+{
+    public class IyzicoPaymentOptions : Options
+    {
+        public const string Key = "IyzicoOptions";
+        public string ThreadsCallbackUrl { get; set; }
+    }
+    public class IyzicoPaymentService : IPaymentService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IyzicoPaymentOptions _options;
+        private readonly IMapper _mapper;
+
+        public IyzicoPaymentService(IConfiguration configuration, IMapper mapper)
+        {
+            _configuration = configuration;
+            _mapper = mapper;
+            var section = _configuration.GetSection(IyzicoPaymentOptions.Key);
+            _options = new IyzicoPaymentOptions()
+            {
+                ApiKey = section["ApiKey"],
+                SecretKey = section["SecretKey"],
+                BaseUrl = section["BaseUrl"],
+            };
+        }
+        private string GenerateConversationId()
+        {
+            return StringHelpers.GenerateUniqueCode();
+        }
+        public InstallmentModel CheckInstallments(string binNumber, decimal price)
+        {
+            var conversationId = GenerateConversationId();
+            if(binNumber.Length <= 6)
+                throw new Exception("Bin number must be at least 6 characters");
+            var request = new RetrieveInstallmentInfoRequest
+            {
+                Locale = Locale.TR.ToString(),
+                ConversationId = conversationId,
+                BinNumber = binNumber.Substring(0, 6),
+                Price = price.ToString(new CultureInfo("en-US")),
+            };
+
+            var result = InstallmentInfo.Retrieve(request, _options);
+            if(result.Status == "failure")
+            {
+                throw new Exception(result.ErrorMessage);
+            }
+
+            if(result.ConversationId != conversationId)
+            {
+                throw new Exception("Hatalı istek oluşturuldu");
+            }
+
+            var resultModel = _mapper.Map<InstallmentModel>(result.InstallmentDetails[0]);
+
+            return resultModel;
+        }
+
+        public PaymentResponseModel Pay(PaymentModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
